b3030269b61e6662e4791e7926c494f8d466547a
JJY <10yuedefeng@163.com>
2019-07-23 20:57:23 +0800

    remove multer

--- express/server.js
+++ express/server.js
@@ -3,12 +3,10 @@ const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
-const multer = require('multer');
 const sqlite = require('sqlite');
 
-
-var dbPromise = sqlite.open('./db/portral.db', {Promise});
-var db;
+// var dbPromise = sqlite.open('./db/portral.db', {Promise});
+// var db;
 
 const router = express.Router();
 router.get('/', (req, res) => {
@@ -17,124 +15,124 @@ router.get('/', (req, res) => {
   res.end();
 });
 
-app.post('/register', async (req, res) => {
-	var user = req.body;
-	if(user){
-		const { email, basicInfo, detail } = user;
-		const { businessForm, industry, subTypeOfIndustry } = basicInfo;
-		const { 
-			addressBuild,
-			addressBuildHana,
-			addressCounty,
-			addressCountyHana,
-			addressPostcode,
-			addressStreetNumber,
-			birth,
-			build,
-			buildHana,
-			companyName,
-			companyNameHana,
-			county,
-			countyHana,
-			firstName,
-			firstNameHana,
-			lastName,
-			lastNameHana,
-			gender,
-			name,
-			number,
-			postcode,
-			streetNumber,
-			tel
-		} = detail;
-		var result = await db.run(`
-			insert into user (
-				addressBuild,
-				addressBuildHana,
-				addressCounty,
-				addressCountyHana,
-				addressPostcode,
-				addressStreetNumber,
-				birth,
-				build,
-				buildHana,
-				companyName,
-				companyNameHana,
-				county,
-				countyHana,
-				firstName,
-				firstNameHana,
-				lastName,
-				lastNameHana,
-				gender,
-				name,
-				number,
-				postcode,
-				streetNumber,
-				tel
-			) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
-			addressBuild,
-			addressBuildHana,
-			addressCounty,
-			addressCountyHana,
-			addressPostcode,
-			addressStreetNumber,
-			birth,
-			build,
-			buildHana,
-			companyName,
-			companyNameHana,
-			county,
-			countyHana,
-			firstName,
-			firstNameHana,
-			lastName,
-			lastNameHana,
-			gender,
-			name,
-			number,
-			postcode,
-			streetNumber,
-			tel
-		);
-		if(result) {
-			res.send({
-				success: true,
-				message: '注册成功'
-			});
-		}
-	}
-})
+// app.post('/register', async (req, res) => {
+// 	var user = req.body;
+// 	if(user){
+// 		const { email, basicInfo, detail } = user;
+// 		const { businessForm, industry, subTypeOfIndustry } = basicInfo;
+// 		const { 
+// 			addressBuild,
+// 			addressBuildHana,
+// 			addressCounty,
+// 			addressCountyHana,
+// 			addressPostcode,
+// 			addressStreetNumber,
+// 			birth,
+// 			build,
+// 			buildHana,
+// 			companyName,
+// 			companyNameHana,
+// 			county,
+// 			countyHana,
+// 			firstName,
+// 			firstNameHana,
+// 			lastName,
+// 			lastNameHana,
+// 			gender,
+// 			name,
+// 			number,
+// 			postcode,
+// 			streetNumber,
+// 			tel
+// 		} = detail;
+// 		var result = await db.run(`
+// 			insert into user (
+// 				addressBuild,
+// 				addressBuildHana,
+// 				addressCounty,
+// 				addressCountyHana,
+// 				addressPostcode,
+// 				addressStreetNumber,
+// 				birth,
+// 				build,
+// 				buildHana,
+// 				companyName,
+// 				companyNameHana,
+// 				county,
+// 				countyHana,
+// 				firstName,
+// 				firstNameHana,
+// 				lastName,
+// 				lastNameHana,
+// 				gender,
+// 				name,
+// 				number,
+// 				postcode,
+// 				streetNumber,
+// 				tel
+// 			) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+// 			addressBuild,
+// 			addressBuildHana,
+// 			addressCounty,
+// 			addressCountyHana,
+// 			addressPostcode,
+// 			addressStreetNumber,
+// 			birth,
+// 			build,
+// 			buildHana,
+// 			companyName,
+// 			companyNameHana,
+// 			county,
+// 			countyHana,
+// 			firstName,
+// 			firstNameHana,
+// 			lastName,
+// 			lastNameHana,
+// 			gender,
+// 			name,
+// 			number,
+// 			postcode,
+// 			streetNumber,
+// 			tel
+// 		);
+// 		if(result) {
+// 			res.send({
+// 				success: true,
+// 				message: '注册成功'
+// 			});
+// 		}
+// 	}
+// })
 
 
-app.post('/sendMail', async (req, res) => {
-	var mail = req.body;
-	if(mail){
-		const { title, content, origin } = mail;
-		var result = await db.run(`
-			insert into email (
-				title,
-				origin,
-				content
-			) values (?, ?, ?)`,
-			title,
-			origin,
-			content
-		);
-		if(result) {
-			res.send({
-				success: true,
-				message: '发送成功'
-			});
-		}
-	}
-})
+// app.post('/sendMail', async (req, res) => {
+// 	var mail = req.body;
+// 	if(mail){
+// 		const { title, content, origin } = mail;
+// 		var result = await db.run(`
+// 			insert into email (
+// 				title,
+// 				origin,
+// 				content
+// 			) values (?, ?, ?)`,
+// 			title,
+// 			origin,
+// 			content
+// 		);
+// 		if(result) {
+// 			res.send({
+// 				success: true,
+// 				message: '发送成功'
+// 			});
+// 		}
+// 	}
+// })
 
-;
+// ;
 
-(async function() {
-	db = await dbPromise
-})()
+// (async function() {
+// 	db = await dbPromise
+// })()
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
